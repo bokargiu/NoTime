25ca512782d1d379fa888b01429dd799e80df6db
bokargiu <erickjstaack07@gmail.com>
2026-06-10 19:21:28 -0300

    Adicionar arquivos de projeto.

---
+++ NoTime.Infrastructure/AppDB.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NoTime.Infrastructure
+{
+    public class AppDB : DbContext
+    {
+        public AppDB(DbContextOptions<AppDB> options) : base(options) { }
+    }
+}
